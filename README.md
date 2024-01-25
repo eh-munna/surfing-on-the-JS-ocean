1ace95baa6775b559769cef072cf5dd71f80d790
ehmunna <emran-hussain.munna-tu-ilmenau.de>
2024-01-25 20:55:58 +0100

    basics of array

--- README.md
+++ README.md
@@ -1,9 +1,15 @@
 <h1 align= "center">Surfing in the JavaScript Ocean</h1>
 
-## An exciting journey to explore JavaScript
+## JavaScript Arrays
 
-### Branch Naming Convention
+### Topics Covered:
 
-- Different topics are organized in separate branches with naming convention: _JS-ocean/[desired-branch-name]_
-
-  - For example, string related basic practice's source codes are shared in: _JS-ocean/string_ branch
+- Creating Arrays
+- Accessing Array Elements
+- Changing an Array Element
+- Converting an Array to a String
+- Arrays are Objects
+- The length Property
+- Looping Array Elements
+- Adding Array Elements using push( )
+- How to Recognize an Array
--- js/index.js
+++ js/index.js
@@ -1,1 +1,39 @@
-console.log('connecting to the js file');
+// JavaScript Arrays
+
+const arr = ['item1', 'item2', 'item3'];
+console.log(arr);
+
+// - Accessing Array Elements
+
+console.log(arr.length);
+
+// - Changing an Array Element
+
+arr[2] = 'item4';
+console.log(arr);
+
+// - Converting an Array to a String
+
+const numArr = [1, 2, 3, 4];
+
+console.log(`Converting array elements to string:`, numArr.toString());
+
+// The length Property
+console.log(`Length of this array (numArr) is: `, numArr.length);
+
+// Looping Array Elements
+
+const fruits = ['Apples', 'Pears', 'Bananas', 'Oranges'];
+for (let i = 0; i < fruits.length; i++) {
+  console.log(`index ${i} has fruit: `, fruits[i]);
+}
+
+// Adding Array Elements using push ( )
+
+fruits.push(['Water Melon', 'Papaya', 'Avocado']);
+// fruits.push(...['Water Melon', 'Papaya', 'Avocado']);
+console.log(fruits.length);
+
+// How to Recognize an Array
+
+console.log(Array.isArray(fruits)); // returns true if it is an array
