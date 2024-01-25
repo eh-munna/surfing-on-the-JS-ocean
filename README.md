de4136ec0a93089aa9c8174fe568350f07fc1781
ehmunna <emran-hussain.munna-tu-ilmenau.de>
2024-01-25 02:03:13 +0100

    JavaScript String Search: explored

--- README.md
+++ README.md
@@ -1,3 +1,14 @@
 <h1 align= "center">Surfing in the JavaScript Ocean</h1>
 
-### JavaScript String Search
+## JavaScript String Search
+
+### Topics Covered:
+
+- JavaScript String indexOf()
+- JavaScript String lastIndexOf()
+- JavaScript String search()
+- JavaScript String match()
+- JavaScript String matchAll()
+- JavaScript String includes()
+- JavaScript String startsWith()
+- JavaScript String endsWith()
--- js/index.js
+++ js/index.js
@@ -1,1 +1,43 @@
-console.log('connecting to the js file');
+// - JavaScript String indexOf()
+
+const text = `Please locate where 'locate' occurs!`;
+console.log(`Index of 'locate' is: ${text.indexOf('locate')}`);
+
+console.log(text.indexOf('where', 15));
+
+// - JavaScript String lastIndexOf()
+console.log(`Last index of 'locate' is: ${text.lastIndexOf('locate')}`);
+console.log(`Backward index of 'locate' is: ${text.lastIndexOf('locate', 15)}`);
+
+// - JavaScript String search()
+
+console.log(
+  `Return the position of the word 'occurs': ${text.search('occurs')}`
+);
+
+// - JavaScript String match()
+
+const textMatch = `The rain in SPAIN stays mainly in the plain`;
+const match = textMatch.match('ain');
+console.log(match);
+
+// - JavaScript String matchAll()
+
+const matchAll = textMatch.matchAll('ain');
+console.log(matchAll);
+
+// - JavaScript String includes()
+
+const getInclude = textMatch.includes('ain');
+console.log(getInclude);
+
+// - JavaScript String startsWith()
+
+let str = `Welcome to the universe`;
+const getStarts = str.startsWith('to');
+console.log(getStarts);
+
+// - JavaScript String endsWith()
+
+const getEnds = str.endsWith('universe');
+console.log(getEnds);
