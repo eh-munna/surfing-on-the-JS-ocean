a9500f861cb644a33b0afb31151401f3a4c46bae
ehmunna <emran-hussain.munna-tu-ilmenau.de>
2024-01-25 02:28:40 +0100

    JavaScript Template Strings: explored

--- js/index.js
+++ js/index.js
@@ -1,1 +1,24 @@
-console.log('connecting to the js file');
+// - Multiline Strings
+
+const multiLines = `
+Line 1
+Line 2
+Line 3
+`;
+console.log(multiLines);
+
+// - Interpolation
+let port = 5500;
+console.log(`Live server is listening on port ${port}`);
+
+// - Variable Substitutions
+
+const firstName = 'Mr Edwin';
+const lastName = 'Simon';
+console.log(`Welcome ${lastName}, ${firstName}`);
+
+// - Expression Substitution
+
+let num1 = 10;
+let num2 = 30;
+console.log(`Total: ${num1 + num2}`);
