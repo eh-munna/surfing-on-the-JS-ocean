7c087c919ba72cb94c1ed82282e427456bf6b162
ehmunna <emran-hussain.munna-tu-ilmenau.de>
2024-01-27 00:35:13 +0100

    array search is practiced

--- README.md
+++ README.md
@@ -1,9 +1,11 @@
 <h1 align= "center">Surfing in the JavaScript Ocean</h1>
 
-## An exciting journey to explore JavaScript
+## JavaScript Array Search
 
-### Branch Naming Convention
+### Topics Covered:
 
-- Different topics are organized in separate branches with naming convention: _JS-ocean/[desired-branch-name]_
-
-  - For example, string related basic practice's source codes are shared in: _JS-ocean/string_ branch
+- JavaScript Array indexOf()
+- JavaScript Array lastIndexOf()
+- JavaScript Array includes()
+- JavaScript Array find()
+- JavaScript Array findLast()
--- js/index.js
+++ js/index.js
@@ -1,1 +1,23 @@
-console.log('connecting to the js file');
+// JavaScript Array Search
+
+// - JavaScript Array indexOf()
+
+const fruits = ['banana', 'apple', 'orange', 'apple', 'papaya'];
+console.log(fruits.indexOf('banana'));
+
+// - JavaScript Array lastIndexOf()
+
+console.log(fruits.lastIndexOf('apple'));
+
+// - JavaScript Array includes()
+
+console.log(fruits.includes('apple'));
+
+const result = fruits.find((fruit) => fruit === 'orange');
+console.log(typeof result);
+
+// - JavaScript Array findLast()
+
+const temp = [27, 28, 30, 40, 42, 35, 30];
+let high = temp.findLast((x) => x > 40);
+console.log(high);
