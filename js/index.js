8778b060a1a616c03e12dc6f24bedccb658b197a
ehmunna <emran-hussain.munna-tu-ilmenau.de>
2024-02-01 16:15:24 +0100

    JS date object practiced

--- js/index.js
+++ js/index.js
@@ -1,1 +1,36 @@
-console.log('connecting to the js file');
+// JavaScript Date Objects
+
+// - Creating Date Objects
+
+const showOutput = document.getElementById('show-output');
+
+const currentDate = new Date();
+const currentYM = new Date(2023, 10); // (2023, November)
+showOutput.innerText = `Today: ${currentDate}`;
+// showOutput.innerText = `Today: ${currentYM}`;
+console.log(currentDate);
+
+// - Previous Century
+
+const getPrevCentury = new Date(78, 4, 5);
+
+const months = [
+  'January',
+  'February',
+  'March',
+  'April',
+  'May',
+  'June',
+  'July',
+  'August',
+  'September',
+  'October',
+  'November',
+  'December',
+];
+
+const year = getPrevCentury.getFullYear();
+const month = months[getPrevCentury.getMonth()];
+const day = getPrevCentury.getDate();
+
+showOutput.innerText = `Last Century: ${day} ${month} ${year}`;
