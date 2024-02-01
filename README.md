224b9a854ca48b9ec6642b967e0cb35651ef152f
ehmunna <emran.h.munna@gmail.com>
2024-02-01 23:33:12 +0100

    javascript math object is explored

--- README.md
+++ README.md
@@ -1,9 +1,13 @@
 <h1 align= "center">Surfing in the JavaScript Ocean</h1>
 
-## An exciting journey to explore JavaScript
+## JavaScript Math Object
 
-### Branch Naming Convention
+### Topics Covered:
 
-- Different topics are organized in separate branches with naming convention: _JS-ocean/[desired-branch-name]_
-
-  - For example, string related basic practice's source codes are shared in: _JS-ocean/string_ branch
+- Math.round();
+- Math.ceil();
+- Math.floor();
+- Math.trunc();
+- Math.pow()
+- Math.sqrt()
+- Math.random()
--- js/index.js
+++ js/index.js
@@ -1,1 +1,35 @@
-console.log('connecting to the js file');
+// JavaScript Math Object
+
+// Math.round();
+
+const num1 = 2.5545;
+const num2 = 3.2345;
+console.log(Math.round(num1));
+
+// rounded up to its nearest integer
+console.log(Math.ceil(num1));
+console.log(Math.ceil(num2));
+
+// rounded down to its nearest integer
+console.log(Math.floor(num1));
+console.log(Math.floor(num2));
+
+// returns the integer part of number
+console.log(Math.trunc(num2));
+
+// Math.pow() : returns the value of something based on the power of something
+
+const _2_ToThePowerOf_8 = Math.pow(2, 8);
+console.log(_2_ToThePowerOf_8);
+
+// Math.sqrt() : returns the square root of
+const squireRootOf_64 = Math.sqrt(64);
+console.log(squireRootOf_64);
+
+// Math.random()
+
+const randomNumber = Math.random();
+console.log(randomNumber);
+
+const randomTo_4 = Math.floor(Math.random() * 4) + 1;
+console.log(randomTo_4);
