b2290d5fa1b6fa9859526ce1acdbc9d00740614a
ehmunna <emran.h.munna@gmail.com>
2024-02-03 23:53:47 +0100

    basic for loop

--- js/index.js
+++ js/index.js
@@ -1,1 +1,15 @@
-console.log('connecting to the js file');
+// JavaScript For Loop
+
+// Basic for loop
+
+const letters = ['a', 'b', 'c', 'd', 'e', 'f'];
+for (let index = 0; index < letters.length; index++) {
+  console.log(letters[index]);
+}
+
+console.log(`----Reverse-----`);
+
+// Reverse for loop
+for (let index = letters.length - 1; index >= 0; index--) {
+  console.log(letters[index]);
+}
