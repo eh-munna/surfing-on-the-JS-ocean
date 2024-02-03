394e0133f0650fdbe36d42ae6d2e5c6f6512f715
ehmunna <emran.h.munna@gmail.com>
2024-02-03 19:55:42 +0100

    js booleans studied

--- js/index.js
+++ js/index.js
@@ -1,1 +1,18 @@
-console.log('connecting to the js file');
+// JavaScript Booleans
+
+// - The Boolean() Function
+
+const isTrueOrFalse = Boolean(2 > 3);
+console.log(isTrueOrFalse);
+
+// Falsy Values
+
+// 0
+//  -0 (minus zero)
+// empty string
+// undefined
+// null
+// NaN
+// false
+
+console.log(Boolean(-1));
