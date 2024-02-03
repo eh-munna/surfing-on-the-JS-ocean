375aa0fd13440ccabd551d1f5e86b77ee6725ad5
ehmunna <emran.h.munna@gmail.com>
2024-02-03 23:09:47 +0100

    JavaScript Conditions

--- js/index.js
+++ js/index.js
@@ -1,1 +1,44 @@
-console.log('connecting to the js file');
+// JavaScript Conditions
+
+// JavaScript if, else, and else if
+
+const age = 19;
+
+if (age === 18) {
+  console.log(`You are adult`);
+} else if (age > 18 && age < 35) {
+  console.log(`You are old`);
+} else {
+  console.log(`You are kid`);
+}
+console.log(`----------------------------------------------------`);
+
+// JavaScript Switch Statement
+
+console.log(`1st switch statement`);
+
+switch (age) {
+  case 18:
+    console.log(`You are adult`);
+    break;
+  case 20:
+    console.log(`You are old`);
+    break;
+  default:
+    console.log(`You are kid`);
+}
+
+console.log(`----------------------------------------------------`);
+console.log(`2nd switch statement`);
+
+switch (true) {
+  case age === 18:
+    console.log(`You are adult`);
+    break;
+  case age > 18 && age < 30:
+    console.log(`You are old`);
+    break;
+  default:
+    console.log(`You are kid`);
+}
+console.log(`----------------------------------------------------`);
