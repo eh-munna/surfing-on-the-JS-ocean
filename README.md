b0f4861017bea2874f50bd4a3d0e1e365e3c373a
ehmunna <emran.h.munna@gmail.com>
2024-02-04 13:48:19 +0100

    javascript loops practiced

--- README.md
+++ README.md
@@ -1,7 +1,14 @@
 <h1 align= "center">Surfing in the JavaScript Ocean</h1>
 
-## JavaScript For Loop
+## JavaScript Loop
 
 ### Topics Covered:
 
-- Basic for loop
+- Basic For Loop
+- While Loop
+- Do While Loop
+  - The Loop will always be executed at least once, even if the condition is false, because the code block is executed before the condition is tested
+- For In Loop
+  - Basically use for object iteration. For iterating over array this loop can be used but not recommended
+- For Of Loop
+  - Use for looping over an Array
--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-// JavaScript For Loop
+// JavaScript Loop
 
 // Basic for loop
 
@@ -12,4 +12,44 @@ console.log(`----Reverse-----`);
 // Reverse for loop
 for (let index = letters.length - 1; index >= 0; index--) {
   console.log(letters[index]);
+}
+
+// while loop
+
+console.log(`----Print Number 1 to 10 using While Loop-----`);
+
+let i = 1;
+
+while (i < 11) {
+  console.log(`The number is: ${i}`);
+  i++;
+}
+
+console.log(`----Do While Loop-----`);
+
+let j = 1;
+do {
+  console.log(`The number is: ${j}`);
+  j++;
+} while (j > 11);
+
+// For In Loop
+console.log(`----For In Loop-----`);
+
+const cars = {
+  name: 'John',
+  brand: 'Doe',
+  model: 2024,
+};
+
+for (const car in cars) {
+  console.log(`${car}: `, cars[car]);
+}
+
+// For of Loop
+console.log(`----For of Loop-----`);
+
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+for (const number of numbers) {
+  console.log(number);
 }
