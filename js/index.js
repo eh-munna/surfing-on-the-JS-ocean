359c155cf8d6b354197eb7b587b932693bec95f0
eh-munna <emran.h.munna@gmail.com>
2024-02-08 00:46:46 +0100

    js class in short

--- js/index.js
+++ js/index.js
@@ -1,1 +1,22 @@
-console.log('connecting to the js file');
+// JavaScript Classes
+
+// Basic Classes
+class Car {
+  constructor(name, model, status) {
+    this.name = name;
+    this.model = model;
+    this.status = status;
+
+    if (status) {
+      this.getStatus();
+    }
+  }
+  getStatus() {
+    return `Congratulations! You have won a car!
+    name: ${this.name}
+    model: ${this.model}`;
+  }
+}
+
+const car = new Car('Audi', 2019, true);
+console.log(car.getStatus());
