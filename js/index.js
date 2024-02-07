e3e3c5ecd005a1b6d2368e5c414dc600aa2f9396
eh-munna <emran.h.munna@gmail.com>
2024-02-07 22:44:04 +0100

    this keyword practiced

--- js/index.js
+++ js/index.js
@@ -1,1 +1,24 @@
-console.log('connecting to the js file');
+// JavaScript this Keyword
+
+// this in a Method
+
+const person = {
+  name: 'John',
+  age: 30,
+
+  sayHello() {
+    return `Hello ${this.name}`;
+  },
+};
+
+console.log(person.sayHello());
+
+// this in Event Handlers
+
+/* 
+
+<button onclick = "this.style.display= 'none'">
+click me to hide
+</button>
+
+*/
