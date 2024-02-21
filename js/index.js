4ef679574ee21e729fe4051f72c2af0204a3602a
eh-munna <emran.h.munna@gmail.com>
2024-02-21 09:01:11 +0100

    dom practice initiated

--- js/index.js
+++ js/index.js
@@ -1,1 +1,12 @@
-console.log('connecting to the js file');
+// JavaScript HTML DOM
+const div = document.getElementById('dom-practice-area');
+const thirdP = div.querySelector('p:nth-child(3)');
+thirdP.id = 'practice';
+if (thirdP) {
+  thirdP.innerText = 'test 3';
+}
+
+const p = document.createElement('p');
+p.id = 'practice2';
+p.innerText = 'test 4';
+div.appendChild(p);
