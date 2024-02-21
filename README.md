023be309859c398b3ed74d3d3fbc28df5799e64b
eh-munna <emran.h.munna@gmail.com>
2024-02-21 18:45:19 +0100

    will back later

--- README.md
+++ README.md
@@ -1,9 +1,7 @@
 <h1 align= "center">Surfing in the JavaScript Ocean</h1>
 
-## An exciting journey to explore JavaScript
+## Browser Object Model
 
-### Branch Naming Convention
+### Topics Covered:
 
-- Different topics are organized in separate branches with naming convention: _JS-ocean/[desired-branch-name]_
-
-  - For example, string related basic practice's source codes are shared in: _JS-ocean/string_ branch
+-
--- js/index.js
+++ js/index.js
@@ -1,1 +1,50 @@
-console.log('connecting to the js file');
+// JS Browser Object Model
+
+const windowWidth = document.getElementById('window-width');
+const windowHeight = document.getElementById('window-height');
+windowWidth.innerText = `Window Width: ${window.innerWidth} px`;
+windowHeight.innerText = `Window Height: ${window.innerHeight} px`;
+
+// open and close window
+
+const openWindow = document.getElementById('open-window');
+const closeWindow = document.getElementById('close-window');
+
+let myWindow;
+
+openWindow.onclick = () => {
+  myWindow = window.open(
+    `https://www.w3schools.com/js/js_window.asp`,
+    '_blank'
+  );
+};
+
+closeWindow.onclick = () => {
+  myWindow.close();
+};
+
+const back = document.getElementById('back');
+const forward = document.getElementById('forward');
+
+back.onclick = () => {
+  history.back();
+};
+
+forward.onclick = () => {
+  history.forward();
+};
+
+let intervalInstance;
+const startTime = () => {
+  intervalInstance = setInterval(() => {
+    let time = new Date().toLocaleTimeString();
+    document.getElementById('time').innerText = time;
+  }, 1000);
+};
+
+const stopTime = () => {
+  clearInterval(intervalInstance);
+};
+
+document.getElementById('start-watch').onclick = startTime;
+document.getElementById('stop-watch').onclick = stopTime;
