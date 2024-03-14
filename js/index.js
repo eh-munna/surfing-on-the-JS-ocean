e38eee434ae7b511c237a3ecb622c46a672d0249
eh-munna <emran.h.munna@gmail.com>
2024-03-14 21:08:37 +0100

    mdn-js-documentation

--- js/index.js
+++ js/index.js
@@ -1,1 +1,10 @@
 console.log('connecting to the js file');
+
+function bankAccount(initBalance) {
+  let balance = initBalance;
+  return function () {
+    return balance;
+  };
+}
+const account = bankAccount(10000);
+console.log(account);
