621cdfb622ad3cb3d13438364ff94f92fede3bb5
eh-munna <emran.h.munna@gmail.com>
2024-03-19 11:45:44 +0100

    prototype practicing

--- js/index.js
+++ js/index.js
@@ -1,1 +1,28 @@
-console.log('connecting to the js file');
+// JS Prototypes
+
+const myObject = {
+  city: 'Berlin',
+  greet() {
+    console.log(`Hello ${this.city}!`);
+  },
+};
+
+const parentObj = {
+  city: 'London',
+  greet() {
+    console.log(`Hello ${this.city}!`);
+  },
+};
+
+function Test() {
+  name: 'test';
+}
+
+Test.prototype = parentObj;
+
+// parentObj.name = Test.prototype;
+
+console.dir(Test.prototype);
+const childObj = Object.create(Test.prototype);
+
+console.log(childObj);
